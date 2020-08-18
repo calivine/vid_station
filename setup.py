1f8a6a405122d8b02a08e74f939b1fd480498e40
Alex Caloggero <a.calog@protonmail.com>
2020-08-17 22:55:02 -0400

    Edited setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='vid_station',
       version='0.1',
       description='video editing cli',
+      long_description=readme(),
+      keywords='video editing editor',
+      author='Alex C',
+      author_email='a.calog@protonmail.com',
+      url='https://github.com/calivine/vid_station',
       include_package_data=True,
       install_requires=['moviepy'],
       entry_points={
